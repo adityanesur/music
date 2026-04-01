2b5c540a9116db30d59fb79997c0355eb5196c87
ADITYA <adityaname.2005@gmail.com>
2026-04-01 23:54:27 +0530

    Add files via upload

---
+++ songs.js
@@ -0,0 +1,435 @@
+// Auto-generated from songs folder
+const songsDatabase = [
+  {
+    "id": 1,
+    "title": "Aarambh Hai Prachand ｜ Full Song ｜ Lyrics Video 2021",
+    "file": "songs/Aarambh hai Prachand ｜ Full Song ｜ Lyrics Video 2021.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 2,
+    "title": "Alladsu Alladsu ｜ Audio Song ｜ Chowka ｜ Kashinath ｜ Prem Kumar ｜Diganth ｜Vijay ｜Prajwal Devraj",
+    "file": "songs/Alladsu Alladsu ｜ Audio Song ｜ Chowka ｜ Kashinath ｜ Prem Kumar ｜Diganth ｜Vijay ｜Prajwal Devraj.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 3,
+    "title": "Ambarsariya    Sona Mohapatra (Lyrics) ｜ Fukrey ｜ Ram Sampath ｜ Rhythm Hub",
+    "file": "songs/Ambarsariya -  sona Mohapatra (lyrics) ｜ Fukrey ｜ Ram Sampath ｜ Rhythm hub.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 4,
+    "title": "Anuv Jain   Jo Tum Mere Ho (Lyrics)",
+    "file": "songs/Anuv Jain - JO TUM MERE HO (Lyrics).mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 5,
+    "title": "Apsara Aali   Lyrical ｜ Natarang ｜ Komal Kaaya Ki Mohamaaya ｜ Sonalee Kulkarni, Ajay Atul",
+    "file": "songs/Apsara Aali - Lyrical ｜ Natarang ｜ Komal Kaaya Ki Mohamaaya ｜ Sonalee Kulkarni, Ajay Atul.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 6,
+    "title": "Aurora   Runaway (Lyrics)",
+    "file": "songs/Aurora - Runaway (Lyrics).mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 7,
+    "title": "Ayogya ｜ Yenammi Yenammi ｜ New 4K Song Lyrics 2018 ｜ Sathish Ninasam ｜ Rachitha Ram ｜ Arjun Janya",
+    "file": "songs/Ayogya ｜ Yenammi Yenammi ｜ New 4K Song Lyrics 2018 ｜ Sathish Ninasam ｜ Rachitha Ram ｜ Arjun Janya.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 8,
+    "title": "Azeem O Shaan Shahenshah ｜ Jodhaa Akbar   Lyrics",
+    "file": "songs/Azeem-O-Shaan Shahenshah ｜ Jodhaa Akbar - Lyrics.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 9,
+    "title": "Banjaare   Bairan (Lyrics)",
+    "file": "songs/Banjaare - Bairan (Lyrics).mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 10,
+    "title": "Bhutto",
+    "file": "songs/Bhutto.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 11,
+    "title": "Bramha Vishnu Shiva Edhe Haalu Kudidaro,  Kannada Song",
+    "file": "songs/Bramha vishnu Shiva edhe haalu kudidaro,  kannada song.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 12,
+    "title": "Care Ni Karda (Lyrics)   Yo Yo Honey Singh, Sweetaj Brar ｜",
+    "file": "songs/Care ni karda (lyrics) - Yo Yo Honey Singh, Sweetaj Brar ｜.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 13,
+    "title": "Character Dheela Lyrics   Ready ｜ Pritam, Neeraj Shridhar, Amrita Kak",
+    "file": "songs/Character Dheela Lyrics - Ready ｜ Pritam, Neeraj Shridhar, Amrita Kak.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 14,
+    "title": "Chenappa Channegowda 🏺  ｜ Diya Hegde'S Soulful Kannada Folk Song ｜  Kannada Folk Music",
+    "file": "songs/Chenappa Channegowda 🏺  ｜ Diya Hegde's Soulful Kannada Folk Song ｜  Kannada Folk Music.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 15,
+    "title": "Chunar Full Song ｜ Disney'S Abcd 2 ｜ Varun Dhawan   Shraddha Kapoor ｜ Arijit Singh ｜ Sachin   Jigar",
+    "file": "songs/Chunar Full Song ｜ Disney's ABCD 2 ｜ Varun Dhawan - Shraddha Kapoor ｜ Arijit Singh ｜ Sachin - Jigar.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 16,
+    "title": "Cycle Cycle Mari Sonani Cycle🌹 Orignal Songs ⧸⧸ Anandilal Bhawel ⧸⧸ Mp Adivasi Songs @Tribal Timli",
+    "file": "songs/Cycle Cycle Mari Sonani Cycle🌹 Orignal Songs ⧸⧸ Anandilal Bhawel ⧸⧸ MP Adivasi Songs @Tribal Timli.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 17,
+    "title": "Dil Toh Bachcha Hai   Lyrical Video",
+    "file": "songs/Dil Toh Bachcha Hai - Lyrical Video.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 18,
+    "title": "Dil Tu Jaan Tu   Arunima Sharma ｜ Gurnazar ｜ Female Version ｜ (Lyrics)",
+    "file": "songs/Dil Tu Jaan Tu - Arunima Sharma ｜ Gurnazar ｜ Female Version ｜ (Lyrics).mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 19,
+    "title": "Dilliwali Girlfriend   Arijit Singh & Sunidhi Chauhan #Bollywoodsongs #Bollywood #Songs #Lyrics",
+    "file": "songs/DILLIWALI GIRLFRIEND - ARIJIT SINGH & SUNIDHI CHAUHAN #bollywoodsongs #bollywood #songs #lyrics.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 20,
+    "title": "Do Patti： Raanjhan (Lyrics)",
+    "file": "songs/Do Patti： Raanjhan (lyrics).mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 21,
+    "title": "Dola Re Dola (Lyrical Video) ｜ Kavita Krishnamurthy ｜ Shreya Ghoshal ｜ K.K. ｜ Devdas",
+    "file": "songs/Dola Re Dola (Lyrical Video) ｜ Kavita Krishnamurthy ｜ Shreya Ghoshal ｜ K.K. ｜ Devdas.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 22,
+    "title": "Duvidha मेरे कृष्ण की ~ Hindi Rap Song ~ Lucke",
+    "file": "songs/Duvidha मेरे कृष्ण की ~ Hindi Rap Song ~ Lucke.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 23,
+    "title": "Ek Ladki Ko Dekha Toh Aisa Laga (Lyrics)   Darshan Raval ｜ Anil ｜ Sonam ｜ Rajkummar",
+    "file": "songs/Ek Ladki Ko Dekha Toh Aisa Laga (Lyrics) - Darshan Raval ｜ Anil ｜ Sonam ｜ Rajkummar.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 24,
+    "title": "Fa9La   Flipperachi (Prod. By Dj Outlaw) (Official Audio) ｜ فصلة   فليبراتشي",
+    "file": "songs/Fa9la - Flipperachi (Prod. by DJ Outlaw) (Official Audio) ｜ فصلة - فليبراتشي.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 25,
+    "title": "Ganesh Aarti From Movie Vaastav I Hindi English Lyrics, Full Lyrical Video I Shendoor Laal Chadhaayo",
+    "file": "songs/Ganesh Aarti from movie VAASTAV I Hindi English Lyrics, Full LYRICAL VIDEO I SHENDOOR LAAL CHADHAAYO.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 26,
+    "title": "Huttiadare Kannada Song Lyrics In Kannada｜Raj Kumar｜Hamsalekha｜Aakasmika @Feelthelyrics",
+    "file": "songs/Huttiadare Kannada Song Lyrics In Kannada｜Raj kumar｜Hamsalekha｜Aakasmika @FeelTheLyrics.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 27,
+    "title": "Ishq Jalakar   Karvaan (Lyrics⧸English Translation)  Dhurandhar ｜ Na Toh Karvan Ki Talash Hai",
+    "file": "songs/Ishq Jalakar - Karvaan (Lyrics⧸English Translation)- Dhurandhar ｜ na toh karvan ki talash hai.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 28,
+    "title": "Jaani, Mohammad Faiz   Dekhha Tenu (Lyrics) ｜ 7Clouds Hindi",
+    "file": "songs/Jaani, Mohammad Faiz - Dekhha Tenu (Lyrics) ｜ 7clouds Hindi.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 29,
+    "title": "Jugni Bulle Shah Tochi Raina   With Lyrics",
+    "file": "songs/Jugni Bulle Shah Tochi Raina - with Lyrics.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 30,
+    "title": "Jutti Meri Lyrics   Neha Bhasin • Thumak Thumak Janiye Mahiye De Naal",
+    "file": "songs/Jutti Meri Lyrics - Neha Bhasin • thumak thumak janiye mahiye de naal.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 31,
+    "title": "Kan Kar Gal Sun Makhna ｜ Amar Singh Chamkila ｜ Diljit Dosanjh ｜ Parineeti Chopra ｜ Imtiaz Ali",
+    "file": "songs/Kan Kar Gal Sun Makhna ｜ Amar Singh Chamkila ｜ Diljit Dosanjh ｜ Parineeti Chopra ｜ Imtiaz Ali.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 32,
+    "title": "Kanmani Anbodu Kadhalan Sound Check ｜ Kanmani Anbodu Kadhalan Song ｜ Kanmani Anbodu ｜ Dj Shubham K",
+    "file": "songs/Kanmani Anbodu Kadhalan Sound Check ｜ Kanmani Anbodu Kadhalan Song ｜ Kanmani Anbodu ｜ DJ Shubham K.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 33,
+    "title": "Kar Har Maidaan Fateh (Lyrics) ｜ Sanju ｜ Shreya Ghoshal, Ranbir Kapoor, Sanjay Dutt",
+    "file": "songs/Kar Har Maidaan Fateh (Lyrics) ｜ Sanju ｜ Shreya Ghoshal, Ranbir Kapoor, Sanjay Dutt.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 34,
+    "title": "Kon Disa Mein    Varsha Singh ｜ Extended Version ｜ (Lyrics)",
+    "file": "songs/Kon Disa Mein  - Varsha Singh ｜ Extended Version ｜ (Lyrics).mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 35,
+    "title": "Kun Faya Kun Lyrics｜ Rockstar｜Ranbir Kapoor ｜ A.R. Rahman, Javed Ali, Mohitchauhan",
+    "file": "songs/Kun Faya Kun lyrics｜ Rockstar｜Ranbir Kapoor ｜ A.R. Rahman, Javed Ali, MohitChauhan.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 36,
+    "title": "Mere Nishan (Lyrics)   Darshan Raval 🎶 ｜ Jhuki Teri Palko Mein Mil Jaye Mujhe Panahe✨",
+    "file": "songs/Mere Nishan (Lyrics) - Darshan Raval 🎶 ｜ Jhuki teri palko mein mil jaye mujhe panahe✨.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 37,
+    "title": "Moliy   Shake It To The Max (Remix) (Lyrics) Ft. Silent Addy, Skillibeng & Shenseeal",
+    "file": "songs/Moliy - Shake It To The Max (Remix) (Lyrics) FT. Silent Addy, Skillibeng & Shenseeal.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 38,
+    "title": "Munjaane Manjalli Lyrical Song ｜ Kiccha Sudeep ｜ Ramya ｜ Raghu Dixit ｜ Just Maath Maathali",
+    "file": "songs/Munjaane Manjalli Lyrical Song ｜ Kiccha Sudeep ｜ Ramya ｜ Raghu Dixit ｜ Just Maath Maathali.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 39,
+    "title": "Open Hairu Bitkondu ｜ Adhyaksha Hd Kannada Movie Song ｜ Sharan, Raksha ｜ Arjun Janya",
+    "file": "songs/Open Hairu Bitkondu ｜ Adhyaksha HD Kannada Movie song ｜ Sharan, Raksha ｜ Arjun Janya.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 40,
+    "title": "Pardesiya (Film Version) Sidharth M, Janhvi K ｜Param Sundari｜Sachin Jigar, Sonu,Krishnakali,Amitabh",
+    "file": "songs/Pardesiya (Film Version) Sidharth M, Janhvi K ｜Param Sundari｜Sachin-Jigar, Sonu,Krishnakali,Amitabh.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 41,
+    "title": "Paro Lyrics   Aditya Rikhari, Unplg'D • Ke Ab Kuch Hosh Nahi Hai",
+    "file": "songs/Paro Lyrics - Aditya Rikhari, UNPLG'd • ke ab kuch hosh nahi hai.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 42,
+    "title": "Phir Se (Lyrics)   Dhurandhar The Revenge ｜ Ranveer Singh ｜ Arijit Singh ｜ Irshad K, Shashwat S",
+    "file": "songs/Phir Se (LYRICS) - Dhurandhar The Revenge ｜ Ranveer Singh ｜ Arijit Singh ｜ Irshad K, Shashwat S.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 43,
+    "title": "Prem Adda   Kalli Evalu   Lyrics Hd",
+    "file": "songs/PREM ADDA - KALLI EVALU - LYRICS HD.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 44,
+    "title": "Ranjha – Official Video ｜ Shershaah ｜ Sidharth–Kiara ｜ B Praak ｜ Jasleen Royal ｜ Romy ｜ Anvita Dutt",
+    "file": "songs/Ranjha – Official Video ｜ Shershaah ｜ Sidharth–Kiara ｜ B Praak ｜ Jasleen Royal ｜ Romy ｜ Anvita Dutt.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 45,
+    "title": "Sanju Rathod   Shaky (Lyrics) Ft. Isha Malviya, G Spark",
+    "file": "songs/Sanju Rathod - Shaky (Lyrics) ft. Isha Malviya, G-Spark.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 46,
+    "title": "Sanson Ki Mala   Nusrat Fateh Ali Khan (Lyric) ｜ Album 🩷 ｜ Qawali ｜ Rhythm Hub",
+    "file": "songs/Sanson ki Mala - Nusrat fateh ali khan (lyric) ｜ Album 🩷 ｜ Qawali ｜ Rhythm hub.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 47,
+    "title": "Sensation Title Song",
+    "file": "songs/Sensation Title Song.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 48,
+    "title": "Shaba Dholki Maara ｜ Afaq Zafar Ft Rubab Shah ｜ Rap ｜ Pashto New Song 2024 ｜ Wedding Song ｜ 4K",
+    "file": "songs/Shaba Dholki Maara ｜ Afaq Zafar ft Rubab Shah ｜ Rap ｜ Pashto New Song 2024 ｜ Wedding Song ｜ 4k.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 49,
+    "title": "Shakira   Waka Waka (This Time For Africa) (Lyrics)",
+    "file": "songs/Shakira - Waka Waka (This Time For Africa) (Lyrics).mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 50,
+    "title": "Shape Of You",
+    "file": "songs/shape-of-you.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 51,
+    "title": "Shararat Song Lyrics Video｜ Dhurandhar ｜ Ranveer ｜ Aditya Dhar ｜ Shashwat ｜ Jasmine",
+    "file": "songs/Shararat Song Lyrics Video｜ Dhurandhar ｜ Ranveer ｜ Aditya Dhar ｜ Shashwat ｜ Jasmine.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 52,
+    "title": "Shoorveer   A Tribute To महाराणा प्रताप जी  ｜ Rapperiya Baalam ｜ Rajneesh Jaipuri ｜ Honey Trouper",
+    "file": "songs/SHOORVEER - A Tribute to महाराणा प्रताप जी  ｜ Rapperiya Baalam ｜ Rajneesh Jaipuri ｜ Honey Trouper.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 53,
+    "title": "Shoorveer 3   A Tribute To छत्रपति शिवाजी महाराज ｜ Rapperiya Baalam Ft. Shambho I Meetu Solanki",
+    "file": "songs/SHOORVEER 3 - A Tribute to छत्रपति शिवाजी महाराज ｜ Rapperiya Baalam Ft. Shambho I Meetu Solanki.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 54,
+    "title": "Shoorveer Ii   A Tribute To महाराणा प्रताप जी ｜ Rapperiya Baalam Ft. Jagirdar Rv I Album Thar Coast",
+    "file": "songs/SHOORVEER II - A tribute to महाराणा प्रताप जी ｜ Rapperiya Baalam Ft. Jagirdar RV I Album THAR COAST.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 55,
+    "title": "Shubh   One Love (Lyrics)",
+    "file": "songs/Shubh - One Love (Lyrics).mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 56,
+    "title": "Sia   Cheap Thrills (Lyrics) Ft. Sean Paul",
+    "file": "songs/Sia - Cheap Thrills (Lyrics) ft. Sean Paul.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 57,
+    "title": "Sun Saathiya Lyrics",
+    "file": "songs/sun saathiya lyrics.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 58,
+    "title": "Sweetheart   Full Video ｜ Kedarnath ｜ Sushant Singh ｜ Sara Ali Khan ｜ Dev Negi ｜ Amit Trivedi",
+    "file": "songs/Sweetheart - Full Video ｜ Kedarnath ｜ Sushant Singh ｜ Sara Ali Khan ｜ Dev Negi ｜ Amit Trivedi.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 59,
+    "title": "Tamma Tamma Loge Lyrical Video ｜ Thanedaar ｜ Bappi Lahiri ｜ Anuradha Paudwal ｜ Sanjay Dutt, Madhuri",
+    "file": "songs/Tamma Tamma Loge Lyrical Video ｜ Thanedaar ｜ Bappi Lahiri ｜ Anuradha Paudwal ｜ Sanjay Dutt, Madhuri.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 60,
+    "title": "Tera Ghata ｜ Lyrical Video ｜ Gajendra Verma Ft. Karishma Sharma ｜ Vikram Singh",
+    "file": "songs/Tera Ghata ｜ Lyrical Video ｜ Gajendra Verma Ft. Karishma Sharma ｜ Vikram Singh.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 61,
+    "title": "Tera Pani Vi Bharangi Ranjheya Ve (Lyrics) Ranjheya Ve   Zain Zohaib",
+    "file": "songs/Tera Pani Vi Bharangi Ranjheya Ve (Lyrics) Ranjheya Ve - Zain Zohaib.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 62,
+    "title": "Tokyo Drift ｜｜ Lyrics Of Tokyo Drift ｜｜ By Teriyaki Boyz ｜｜ Official Song With Lyrics",
+    "file": "songs/Tokyo Drift ｜｜ Lyrics of Tokyo Drift ｜｜ By Teriyaki Boyz ｜｜ Official Song with lyrics.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 63,
+    "title": "Tu Jaane Na   Atif Aslam (Lyrics) ｜ Lyrical Bam Hindi",
+    "file": "songs/Tu Jaane Na - Atif Aslam (Lyrics) ｜ Lyrical Bam Hindi.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 64,
+    "title": "Tunak Tunak Tun Video ｜ Daler Mehndi ｜ Full Song ｜ Daler Mehndi Music",
+    "file": "songs/Tunak Tunak Tun Video ｜ Daler Mehndi ｜ Full Song ｜ Daler Mehndi Music.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 65,
+    "title": "Varanasi： Rana Kumbha (Audio) ｜Mahesh Babu ｜Priyanka Chopra ｜Prithviraj ｜Ss Rajamouli ｜Mm Keeravaani",
+    "file": "songs/Varanasi： Rana Kumbha (Audio) ｜Mahesh Babu ｜Priyanka Chopra ｜Prithviraj ｜SS Rajamouli ｜MM Keeravaani.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 66,
+    "title": "Ve Maahi  Lyrics   Arijit Singh & Asees Kaur ｜ Kesari",
+    "file": "songs/Ve Maahi  Lyrics - Arijit Singh & Asees Kaur ｜ Kesari.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 67,
+    "title": "Ye Tune Kya Kiya   Javed Bashir (Lyrics) ｜ Lyrical Bam Hindi",
+    "file": "songs/Ye Tune Kya Kiya - Javed Bashir (Lyrics) ｜ Lyrical Bam Hindi.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 68,
+    "title": "Yugat Mandli Lyrical Song ｜ Pawankhind ｜ Chinmay Mandlekar ｜ Digpal Lanjekar ｜ Devdutta Baji",
+    "file": "songs/Yugat Mandli Lyrical Song ｜ Pawankhind ｜ Chinmay Mandlekar ｜ Digpal Lanjekar ｜ Devdutta Baji.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 69,
+    "title": "Zingaat   Lyrical ｜ Sairat ｜ Akash Thosar & Rinku Rajguru ｜ Ajay Atul ｜ Nagraj Manjule",
+    "file": "songs/Zingaat - Lyrical ｜ Sairat ｜ Akash Thosar & Rinku Rajguru ｜ Ajay Atul ｜ Nagraj Manjule.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 70,
+    "title": "Zoobi Doobi Lyrics   3 Idiots ｜ Sonu Nigam, Shreya Ghoshal ｜ Trending Hindi Song 2024",
+    "file": "songs/Zoobi Doobi Lyrics - 3 Idiots ｜ Sonu Nigam, Shreya Ghoshal ｜ Trending Hindi Song 2024.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 71,
+    "title": "ಬಿಟ್ಟು ಹೊಂಟ್ಯೆಲ್ಲಾ ನನಹಳ್ಳಿ ｜ Bittu Hontyella Nana Halli ｜ Shabbir Dange ｜ B.R.Chaya ｜ Folk Song",
+    "file": "songs/ಬಿಟ್ಟು ಹೊಂಟ್ಯೆಲ್ಲಾ ನನಹಳ್ಳಿ ｜ Bittu Hontyella Nana Halli ｜ Shabbir Dange ｜ B.R.Chaya ｜ Folk Song.mp3",
+    "duration": "0:00"
+  },
+  {
+    "id": 72,
+    "title": "＂Kabira Full Lyrics Song＂ Yeh Jawaani Hai Deewani｜ Ranbir Kapoor, Deepika Padukone",
+    "file": "songs/＂Kabira Full Lyrics Song＂ Yeh Jawaani Hai Deewani｜ Ranbir Kapoor, Deepika Padukone.mp3",
+    "duration": "0:00"
+  }
+];
